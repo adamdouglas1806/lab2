4bda67b097ea3eb86d3bd41c28745445dc6bfd59
ad3017 <ad3017@HW-GRDL-PC075.hw.ac.uk>
2024-09-24 11:21:38 +0100

    I completed the BMI task without the optional specification

--- src/BmiCalculator.java
+++ src/BmiCalculator.java
@@ -1,9 +1,47 @@
-
+import java.util.Scanner;
 public class BmiCalculator {
 
 	public static void main(String[] args) {
-		// TODO Write your BMI calculator code
+		System.out.println("Welcome to the HW-BMI calculator");
+		
+		System.out.println("================================");	
+			
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Please enter your height(m)");
+		double height = scanner.nextDouble();
 
+		System.out.println("Please enter your weight(kg)");
+		int weight = scanner.nextInt();
+		
+		double BMI = weight/(height*height);
+		
+		System.out.println("Your BMI is -> "+BMI);
+		
+		System.out.println("Your height in (meter) "+height);
+		
+		System.out.println("Your weight in (kilograms) "+weight);
+		
+		double heightF = (height*3.28);
+		
+		System.out.println("Your height in (feet) "+heightF);
+		
+		double weightP = (weight*2.2);
+		
+		System.out.println("Your weight in (pound) "+weightP);
+		
+		if (BMI < 18.5) {
+			System.out.println("You are Underweight!");
+		}
+		
+		if ((BMI >= 18.5)&&(BMI <= 24.9)) {
+			System.out.println("You are Healthy!");
+		}
+		
+		if (BMI > 24.9) {
+			System.out.println("You are Overweight!");
+		}
+		
 	}
 
 }
